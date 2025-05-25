b385d7b7f5c074118379899652cf100950796131
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-05-24 22:45:15 -0700

    chore: move microservice server startup logic from deployment workflow to Shell script

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo ""
+echo "🛑 Stopping existing microservice deployment ..."
+pkill -f market-service || true
+echo "✅ Done."
+
+echo ""
+echo "⚒️ Building new executable..."
+go build -o market-service main.go
+echo "✅ Done."
+
+echo ""
+echo "🚀 Running executable..."
+nohup ./market-service >> market.log 2>&1 &
+echo "✅ Done."
+
+echo ""
+echo "🎉 Microservice up and running."
+echo ""
