6b1ce3f1585f0cc24a3993629a5d1c9ad182cb32
jakubstetz <hugo.jakub.stetz@gmail.com>
2025-05-28 21:05:57 -0700

    refactor: pull condition parameter out of error handling function

--- utils.go
+++ utils.go
@@ -5,15 +5,10 @@ import (
 	"net/http"
 )
 
-func RespondIfError(condition bool, w http.ResponseWriter, message string, statusCode int) bool {
-	if !condition {
-		return false
-	}
-
+func GenerateError(w http.ResponseWriter, message string, statusCode int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(map[string]string{
 		"detail": message,
 	})
-	return true
 }
